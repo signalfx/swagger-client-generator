1394c978c9de2baf723e2bcc4b5104bc1d319c24
FedeG <federicog@exo.com.ar>
2015-07-16 16:35:44 -0300

    Update example (http://petstore.swagger.wordnik.com/api/api-docs -> http://petstore.swagger.io/api/api-docs)

--- example/schemaUpdater.js
+++ example/schemaUpdater.js
@@ -1,9 +1,9 @@
 // run npm install fetch-swagger-schema first
-var path = require('path'),
-  fs = require('fs'),
-  fetchSchema = module.require('fetch-swagger-schema');
+var path = require('path');
+var fs = require('fs');
+var fetchSchema = module.require('fetch-swagger-schema');
 
-var apiDocs = 'http://petstore.swagger.wordnik.com/api/api-docs';
+var apiDocs = 'http://petstore.swagger.io/api/api-docs';
 var destination = __dirname + '/schema.json';
 
 fetchSchema(apiDocs, function(err, schema){
